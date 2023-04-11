7caf6ed0fc02d147050230c18104abcafb4f200d
Nikita Shpikin <nikita.shpikin@gmail.com>
2023-04-11 16:36:26 +0500

    реализовал плавный скролл

--- components/header/header.js
+++ components/header/header.js
@@ -1,9 +1,23 @@
 let burger = document.querySelector('.header__burger');
 
-burger.addEventListener('click', function (e) {
-	document.querySelector('.header__nav').classList.toggle('header__nav_active');
+export function headerMenu() {
+	burger.addEventListener('click', function (e) {
+		document
+			.querySelector('.header__nav')
+			.classList.toggle('header__nav_active');
 
-	document
-		.querySelector('.header__burger')
-		.classList.toggle('header__burger_active');
-});
+		document
+			.querySelector('.header__burger')
+			.classList.toggle('header__burger_active');
+	});
+}
+
+export function smoothScroll(e) {
+	// передать секцию!
+
+	window.scroll({
+		left: 0,
+		top: e.offsetTop,
+		behavior: 'smooth',
+	});
+}
---
+++ script.js
@@ -0,0 +1,20 @@
+import { headerMenu, smoothScroll } from './components/header/header.js';
+
+headerMenu();
+
+const headerLinks = document.querySelectorAll('.header__link');
+
+headerLinks.forEach(link => {
+	link.addEventListener('click', e => {
+		e.preventDefault();
+
+		let href = e.target.getAttribute('href').substring(1);
+		let sections = document.querySelectorAll('section');
+
+		sections.forEach(section => {
+			if (section.getAttribute('id') == href) {
+				smoothScroll(section);
+			}
+		});
+	});
+});
